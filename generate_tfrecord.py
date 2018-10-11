a74a5bfc6c92fe14dec4f2279ebfbf05ed4bb385
TYZHAO <zhaotianyu0121@sina.com>
2018-10-11 16:18:56 +0800

    debug dir

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
-writer = tf.python_io.TFRecordWriter('test.tfrecord')
-clips = 4
+writer = tf.python_io.TFRecordWriter('/beegfs/rw1691/inputs.tfrecord')
+clip_num = 20
 tf.reset_default_graph()
+
 file_name = '/home/rw1691/2018summer/trainlist.txt'
 
 train_img = []
@@ -12,22 +13,19 @@ for line in open(file_name):
     line = line.split()
     train_img.append(line)
 
-#train_img = [item[0] for item in train_img]
+train_img = [train_img[x:x+clip_num] for x in range(0, len(train_img), clip_num)]
 
-train_img = [train_img[x:x+clips] for x in range(0, len(train_img), clips)]
-batch_data = []
 print(len(train_img))
 for index,clip in enumerate(train_img):
-    cc = []
+    clips = []
     for img in clip:
         std_img = cv2.resize(cv2.imread(str(img[0])),(224,224))
         img_param = [int(cv2.IMWRITE_JPEG_QUALITY), 95]
         _, img_encode = cv2.imencode('.jpg', std_img, img_param)
         std_img = tf.compat.as_bytes(img_encode.tostring())
-        batch_data.append(std_img)
+        clips.append(std_img)
 
-
-    input_features = [tf.train.Feature(bytes_list = tf.train.BytesList(value=[item])) for item in batch_data]
+    input_features = [tf.train.Feature(bytes_list = tf.train.BytesList(value=[item])) for item in clips]
     feature_list = {
       'inputs': tf.train.FeatureList(feature=input_features)
     }
