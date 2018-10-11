f7eaf9fab0779912a4f7c572f3b68e8e734d0f28
TYZHAO <zhaotianyu0121@sina.com>
2018-10-11 16:01:41 +0800

    add code for generate tfrecord

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,24 +1,40 @@
 import tensorflow as tf
 import numpy as np
+import cv2
 
 writer = tf.python_io.TFRecordWriter('test.tfrecord')
+clips = 4
+tf.reset_default_graph()
+#file_name = '/home/rw1691/2018summer/trainlist.txt'
+file_name = 'F:\\data\\ucf101_jpegs_256.zip~\\train_list.txt'
+train_img = []
+for line in open(file_name):
+    line = line.split()
+    train_img.append(line)
 
-raw = open('1.jpg','rb').read()
-anzin = open('2.jpg','rb').read()
-rw = open('3.jpg','rb').read()
-c = tf.compat.as_bytes(raw)
-d = tf.compat.as_bytes(anzin)
-e = tf.compat.as_bytes(rw)
-input_features = [tf.train.Feature(bytes_list = tf.train.BytesList(value=[c])),tf.train.Feature(bytes_list = tf.train.BytesList(value=[d])),
-                  tf.train.Feature(bytes_list = tf.train.BytesList(value=[e]))]
-print(type(input_features[0]))
-feature_list = {
-  'inputs': tf.train.FeatureList(feature=input_features)
-}
-feature_lists = tf.train.FeatureLists(feature_list=feature_list)
-example = tf.train.SequenceExample(feature_lists=feature_lists)
+#train_img = [item[0] for item in train_img]
 
-serialized = example.SerializeToString()
-writer.write(serialized)
+train_img = [train_img[x:x+clips] for x in range(0, len(train_img), clips)]
+batch_data = []
+print(len(train_img))
+for index,clip in enumerate(train_img):
+    cc = []
+    for img in clip:
+        std_img = cv2.resize(cv2.imread(str(img[0])),(224,224))
+        img_param = [int(cv2.IMWRITE_JPEG_QUALITY), 95]
+        _, img_encode = cv2.imencode('.jpg', std_img, img_param)
+        std_img = tf.compat.as_bytes(img_encode.tostring())
+        batch_data.append(std_img)
 
+
+    input_features = [tf.train.Feature(bytes_list = tf.train.BytesList(value=[item])) for item in batch_data]
+    feature_list = {
+      'inputs': tf.train.FeatureList(feature=input_features)
+    }
+    feature_lists = tf.train.FeatureLists(feature_list=feature_list)
+    example = tf.train.SequenceExample(feature_lists=feature_lists)
+
+    serialized = example.SerializeToString()
+    writer.write(serialized)
+    print('finish write to {}'.format(index))
 writer.close()
