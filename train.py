3bbe9b92ac2cb755792568bc7d2d88a8c3e6a26b
TYZHAO <zhaotianyu0121@sina.com>
2018-10-12 10:37:19 +0800

    multi gpu

--- train.py
+++ train.py
@@ -36,56 +36,58 @@ def lr_schedule(epoch):
 
 def model(x, num_hidden=512):
     #x = tf.zeros((10,5,32,32,3))
-    x = tf.reshape(x, [-1,224,224,3])
-    x = conv_block(x, 'conv1', filters=32, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
-    x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
-    sc1 = x # 112
-    x = conv_block(x, 'conv2', filters=64, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
-    x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
-    sc2 = x # 56
-    x = conv_block(x, 'conv3', filters=128, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
-    x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
-    sc3 = x # 28
-    x = conv_block(x, 'conv4', filters=256, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
-    x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
-    sc4 = x # 14
-    x = conv_block(x, 'conv5', filters=512, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
-    x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
-    sc5 = x # 7
-    x = tf.layers.average_pooling2d(x, pool_size=7, strides=7)
-    x = tf.contrib.layers.flatten(x)
-    #print(x)
-    x = tf.reshape(x, [-1, clips, 512])
-    inputs = []
-    for i in range(clips): 
-        c=x[:,i,:]
-        inputs.append(c)
-    #print(inputs)
-    lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
-    o, s = rnn.static_rnn(lstm_cell, inputs, dtype=tf.float32)
+    with tf.device('/device:GPU:0'):
+        x = tf.reshape(x, [-1,224,224,3])
+        x = conv_block(x, 'conv1', filters=32, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+        sc1 = x # 112
+        x = conv_block(x, 'conv2', filters=64, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+        sc2 = x # 56
+        x = conv_block(x, 'conv3', filters=128, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+        sc3 = x # 28
+        x = conv_block(x, 'conv4', filters=256, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+        sc4 = x # 14
+        x = conv_block(x, 'conv5', filters=512, kernel_size=3, strides=1, activation=tf.nn.relu,batch_normalization=True)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+        sc5 = x # 7
+        x = tf.layers.average_pooling2d(x, pool_size=7, strides=7)
+        x = tf.contrib.layers.flatten(x)
+        #print(x)
+        x = tf.reshape(x, [-1, clips, 512])
+        inputs = []
+        for i in range(clips): 
+            c=x[:,i,:]
+            inputs.append(c)
+        #print(inputs)
+        lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+        o, s = rnn.static_rnn(lstm_cell, inputs, dtype=tf.float32)
 
-    o = tf.stack(o,1)
-    o = tf.expand_dims(o,-2)
-    o = tf.expand_dims(o,-2)
+    with tf.device('/device:GPU:1'):
+        o = tf.stack(o,1)
+        o = tf.expand_dims(o,-2)
+        o = tf.expand_dims(o,-2)
 
-    o = tf.reshape(o, (-1,1,1,512))
-    #stacked = o.get_shape().as_list()
-    #print(stacked)
-    #print(tf.concat([o,sc4],-1))
-    upsample = tf.keras.layers.UpSampling2D(size=(7,7))
-    o = upsample(o)
-    shape = tf.shape(o)    
-    
-    kernel = tf.get_variable('k', (3,3,256,512*2))
-    o = tf.nn.conv2d_transpose(tf.concat([o,sc5],-1), kernel, tf.stack((shape[0],14,14,256)), strides=[1,2,2,1], padding='SAME')
-    kerkernel = tf.get_variable('skr', (3,3,128,256*2))
-    o = tf.nn.conv2d_transpose(tf.concat([o,sc4],-1), kerkernel, tf.stack((shape[0],28,28,128)), strides=[1,2,2,1], padding='SAME')
-    kerrkernel = tf.get_variable('skrr', (3,3,64,128*2))
-    o = tf.nn.conv2d_transpose(tf.concat([o,sc3],-1), kerrkernel, tf.stack((shape[0],56,56,64)), strides=[1,2,2,1], padding='SAME')
-    kerrrkernel = tf.get_variable('skrrr', (3,3,32,64*2))
-    o = tf.nn.conv2d_transpose(tf.concat([o,sc2],-1), kerrrkernel, tf.stack((shape[0],112,112,32)), strides=[1,2,2,1], padding='SAME')
-    kerrrrkernel = tf.get_variable('skrrrr', (3,3,3,32*2))
-    o = tf.nn.conv2d_transpose(tf.concat([o,sc1],-1), kerrrrkernel, tf.stack((shape[0],224,224,3)), strides=[1,2,2,1], padding='SAME')
+        o = tf.reshape(o, (-1,1,1,512))
+        #stacked = o.get_shape().as_list()
+        #print(stacked)
+        #print(tf.concat([o,sc4],-1))
+        upsample = tf.keras.layers.UpSampling2D(size=(7,7))
+        o = upsample(o)
+        shape = tf.shape(o)    
+        
+        kernel = tf.get_variable('k', (3,3,256,512*2))
+        o = tf.nn.conv2d_transpose(tf.concat([o,sc5],-1), kernel, tf.stack((shape[0],14,14,256)), strides=[1,2,2,1], padding='SAME')
+        kerkernel = tf.get_variable('skr', (3,3,128,256*2))
+        o = tf.nn.conv2d_transpose(tf.concat([o,sc4],-1), kerkernel, tf.stack((shape[0],28,28,128)), strides=[1,2,2,1], padding='SAME')
+        kerrkernel = tf.get_variable('skrr', (3,3,64,128*2))
+        o = tf.nn.conv2d_transpose(tf.concat([o,sc3],-1), kerrkernel, tf.stack((shape[0],56,56,64)), strides=[1,2,2,1], padding='SAME')
+        kerrrkernel = tf.get_variable('skrrr', (3,3,32,64*2))
+        o = tf.nn.conv2d_transpose(tf.concat([o,sc2],-1), kerrrkernel, tf.stack((shape[0],112,112,32)), strides=[1,2,2,1], padding='SAME')
+        kerrrrkernel = tf.get_variable('skrrrr', (3,3,3,32*2))
+        o = tf.nn.conv2d_transpose(tf.concat([o,sc1],-1), kerrrrkernel, tf.stack((shape[0],224,224,3)), strides=[1,2,2,1], padding='SAME')
     return o
 
 def _parse_function(serialized_example):
@@ -106,28 +108,30 @@ def _decode_function(bytes_input):
 def train():
     tf.reset_default_graph()
 
-    dataset = tf.data.TFRecordDataset(file_name)
-    dataset = dataset.map(_parse_function)
-    dataset = dataset.repeat(maxepochs)
-    dataset = dataset.batch(batchsize)
-    iterator = dataset.make_one_shot_iterator()
-    next_element = iterator.get_next()
-    
-    x = tf.placeholder(tf.float32, shape=(None,None,224,224,3))    
-    
-    global_step=tf.get_variable('global_step',(), trainable=False, initializer=tf.constant_initializer([1]))
-    global_step_update = tf.assign(global_step, global_step+1)
+    with tf.device('/device:GPU:0'):
+        dataset = tf.data.TFRecordDataset(file_name)
+        dataset = dataset.map(_parse_function)
+        dataset = dataset.repeat(maxepochs)
+        dataset = dataset.batch(batchsize)
+        iterator = dataset.make_one_shot_iterator()
+        next_element = iterator.get_next()
+        
+        x = tf.placeholder(tf.float32, shape=(None,None,224,224,3))    
 
     o = model(x, num_hidden=512)
-    o = tf.reshape(o,tf.shape(x))
-    loss = tf.losses.mean_squared_error(x[:,1:],o[:,:-1])
-    epoch = tf.ceil(global_step*batchsize//85118+1)
-    lr = lr_schedule(epoch)
-    optimizer = tf.train.AdamOptimizer(lr)
+    
+    with tf.device('/device:GPU:1'):
+        o = tf.reshape(o,tf.shape(x))
+        loss = tf.losses.mean_squared_error(x[:,1:],o[:,:-1])
+        epoch = tf.ceil(global_step*batchsize//85118+1)
+        lr = lr_schedule(epoch)
+        optimizer = tf.train.AdamOptimizer(lr)
 
-    optimizer = tf.train.AdamOptimizer()
-    train_op = optimizer.minimize(loss=loss, global_step=global_step)
-
+        optimizer = tf.train.AdamOptimizer()
+        train_op = optimizer.minimize(loss=loss, global_step=global_step)
+        
+        global_step=tf.get_variable('global_step',(), trainable=False, initializer=tf.constant_initializer([1]))
+        global_step_update = tf.assign(global_step, global_step+1)
     init_op = tf.global_variables_initializer()
 
     timestamp = calendar.timegm(time.gmtime())
